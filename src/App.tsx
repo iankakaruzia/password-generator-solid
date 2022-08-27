45e16b92fd8abddc3dceb8f202eb90c27c2a7bdb
Ianka Karuzia <iankakaruzia@gmail.com>
2022-08-26 23:44:12 -0300

    feat: add custom tailwind config

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import type { Component } from 'solid-js'
 
 const App: Component = () => {
-  return (
-    <p class='py-20 text-center text-4xl text-green-700'>Password Generator</p>
-  )
+  return <p class='py-20 text-center'>Password Generator</p>
 }
 
 export default App
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,32 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ['JetBrains Mono', 'monospace']
+      },
+      fontSize: {
+        'heading-large': ['2rem', '2.688rem'],
+        'heading-medium': ['1.5rem', '1.938rem'],
+        'body-medium': ['1.125rem', '1.438rem'],
+        'body-small': ['1rem', '1.32rem']
+      },
+      colors: {
+        gray: {
+          200: '#E6E5EA',
+          500: '#817D92',
+          800: '#24232C',
+          900: '#18171F'
+        },
+        green: {
+          500: '#A4FFAF'
+        },
+        red: {
+          500: '#F64A4A'
+        },
+        orange: {
+          500: '#FB7C58'
+        },
+        yellow: {
+          500: '#F8CD65'
+        }
       }
     }
   },
